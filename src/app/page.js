858b8758e2f68b7de6e694af60b12af354847e66
Naz <163540562+nazeeha-kb@users.noreply.github.com>
2026-04-23 20:39:43 +0300

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
